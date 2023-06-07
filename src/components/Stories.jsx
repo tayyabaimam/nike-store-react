bb7e9bb41c83e00a37ab84e71cd920268c94dc7d
Qasim <qasimriaz814@gmail.com>
2023-06-07 10:31:12 +0500

    Update Stories.jsx

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Splide, SplideSlide } from "@splidejs/react-splide";
-import "@splidejs/splide/dist/css";
+import { Splide, SplideSlide } from "/node_modules/@splidejs/react-splide";
+import "@splidejs/splide/css";
 import { HashtagIcon, HeartIcon } from "@heroicons/react/24/solid";
 import { ClockIcon } from "@heroicons/react/24/outline";
 import Title from "./utils/Title";
